5335cbb1ef6516430d591f73c93fdfec86122651
fantangshan_180503 <393584307@qq.com>
2025-01-28 20:33:17 +0800

    接 Install Release 流程;

---
+++ Build/Linux/Release.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export ProjectDirPath=../../../../../Project/CommandLineArgParser
+../../../ThirdParty/CMakeProjectFramework/PlatformSpecificScript/Linux/Release.sh
---
+++ Build/Linux/Update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export ProjectDirPath=../../../../../Project/CommandLineArgParser
+../../../ThirdParty/CMakeProjectFramework/PlatformSpecificScript/Linux/Update.sh
